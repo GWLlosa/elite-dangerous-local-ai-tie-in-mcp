f2531acc9db169020226146d93295846f230c256
Guillermo Llosa <GWLlosa@gmail.com>
2025-09-06 01:14:41 -0600

    feat: create EDCoPilot integration package
    - Initialize EDCoPilot package for custom file generation
    - Add package documentation for dynamic content creation
    - Set up structure for generator and templates modules
    - Include placeholder for template and generator class exports
    
    Part of Milestone 1: Project initialization

---
+++ src/edcopilot/__init__.py
@@ -0,0 +1,21 @@
+"""
+EDCoPilot Integration Package
+
+This package handles generation of custom EDCoPilot files based on Elite Dangerous
+gameplay data, creating dynamic chatter, crew dialogue, and speech extensions.
+
+Modules:
+- generator: EDCoPilot custom file generation
+- templates: Template system for dynamic content creation
+"""
+
+# Package version
+__version__ = "0.1.0"
+
+# Future exports - will be implemented in later milestones
+# from .generator import EDCoPilotGenerator
+# from .templates import EDCoPilotTemplates, ChitChatTemplate, SpaceChatterTemplate
+
+__all__ = [
+    # Will be populated as classes are implemented
+]
